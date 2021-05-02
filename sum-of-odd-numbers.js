c08dc2773c5a8213bed41720c74bbeceda8cf60e
amos-kibet <kibetamos97@gmail.com>
2021-05-02 07:41:30 +0300

    Create a basic solution to the challenge

---
+++ sum-of-odd-numbers.js
@@ -0,0 +1,10 @@
+/*
+Coding challenge #7:
+Calculate the sum of odd numbers greater than 10 and less than 30
+ */
+var sum = 0;
+
+for (var i = 11; i < 30; i += 2) {
+    sum +=i;
+}
+console.log(sum);
