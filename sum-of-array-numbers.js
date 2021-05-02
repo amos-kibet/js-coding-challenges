a05c41e5406d8fa583221290a35fca0cadcff299
amos-kibet <kibetamos97@gmail.com>
2021-05-02 11:02:03 +0300

    Change the sample values in the array

--- sum-of-array-numbers.js
+++ sum-of-array-numbers.js
@@ -2,7 +2,7 @@
 Coding challenge #10:
 Calculate the sum of numbers in an array of numbers
  */
-var arr = [2,4,5,6,7,8,92,3,4,5,6]; // example array
+var arr = [2, 3, -1, 5, 7, 9, 10, 15, 95]; // example array
 var sum = 0;
 for (var i = 0; i<arr.length; i++) {
     sum += arr[i];
