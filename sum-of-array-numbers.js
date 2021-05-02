41ee426f1e7fd36a68505eeda8634430827f20f8
amos-kibet <kibetamos97@gmail.com>
2021-05-02 10:52:30 +0300

    Compute sum of items in an array

---
+++ sum-of-array-numbers.js
@@ -0,0 +1,10 @@
+/*
+Coding challenge #10:
+Calculate the sum of numbers in an array of numbers
+ */
+var arr = [2,4,5,6,7,8,92,3,4,5,6]; // example array
+var sum = 0;
+for (var i = 0; i<arr.length; i++) {
+    sum += arr[i];
+}
+console.log(sum);
