f1fe5b3f720010b0040b5438d2ffb3d775407413
amos-kibet <kibetamos97@gmail.com>
2021-05-02 10:39:04 +0300

    Implement a function that takes a formula and returns the answer

---
+++ fahrenheit-to-celsius.js
@@ -0,0 +1,10 @@
+/*
+Coding challenge #8:
+Create a function that will convert from Fahrenheit to Celsius
+ */
+Fahrenheit = (F) =>{
+    return (F -32) * 5/9;
+};
+var degrees = Fahrenheit(0);
+
+console.log(degrees);
